eef82d549a7830509a67b808e3fa9c0119e0585b
Joao Lima <jplima1853@gmail.com>
2025-01-16 13:25:29 -0300

    implementando sorteio e funções para atualização, criação e deleção da lista

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // capturando os elementos html com js
 let btnAddAmigo = document.getElementById('btnAdd');
-let btnSortearAmigo = document.getElementById('btnDraw'); 
+let btnSortearAmigo = document.getElementById('btnDraw');
+let campoListaAmigos = document.getElementById('listaAmigos')
 
 // inserindo eventos de clique nos respectivos botões
 if (btnAddAmigo != null) { btnAddAmigo.addEventListener('click', adicionarAmigo); }
@@ -15,17 +16,45 @@ let listAmigos = [];
 //     document.getElementById(id).value = '';
 // }
 
+function gerarListItem(content) {
+    let li = document.createElement('li');
+    li.innerHTML = content;
+
+    return li;
+}
+
+function atualizarLista(amigo) {
+    campoListaAmigos.appendChild(gerarListItem(amigo));
+    listAmigos.push(amigo);
+}
+
+function limparLista() {
+    campoListaAmigos.innerHTML = '';
+    listAmigos = [];
+}
+
 function adicionarAmigo() {
     let txtAmigo = document.getElementById('amigo');
 
     if(txtAmigo.value == null || txtAmigo.value == '') {
         alert('Por favor, insira um nome válido!');
     } else {
-        listAmigos.push(txtAmigo.value);
+        atualizarLista(txtAmigo.value);
         txtAmigo.value = '';
     }
-}   
+}  
 
-function sortearAmigo() {    
+function sortearAmigo() {
+    if (campoListaAmigos.innerHTML == '' || campoListaAmigos.innerHTML == null) {
+        alert('A lista de nomes está vazia!');
+    } else {
+        let campoResultado = document.getElementById('resultado');
+        campoResultado.innerHTML = '';
 
+        let idx = parseInt(Math.random() * listAmigos.length);
+        
+        campoResultado.appendChild(gerarListItem(listAmigos[idx]));
+        limparLista();
+    }
+    
 }
