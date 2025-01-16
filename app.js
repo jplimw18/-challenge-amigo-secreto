573e154449d1767f8fcd16284f0e7210157e1ebd
Joao Lima <jplima1853@gmail.com>
2025-01-16 12:56:43 -0300

    criando função para agregar os amigos

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// capturando os elementos html com js
+let btnAddAmigo = document.getElementById('btnAdd');
+let btnSortearAmigo = document.getElementById('btnDraw'); 
+
+// inserindo eventos de clique nos respectivos botões
+if (btnAddAmigo != null) { btnAddAmigo.addEventListener('click', adicionarAmigo); }
+if (btnSortearAmigo != null) { btnSortearAmigo.addEventListener('click', sortearAmigo); }
+
+
+// ------
+
+let listAmigos = [];
+
+// function limpar() {
+//     document.getElementById(id).value = '';
+// }
+
+function adicionarAmigo() {
+    let txtAmigo = document.getElementById('amigo');
+
+    if(txtAmigo.value == null || txtAmigo.value == '') {
+        alert('Por favor, insira um nome válido!');
+    } else {
+        listAmigos.push(txtAmigo.value);
+        txtAmigo.value = '';
+    }
+}   
+
+function sortearAmigo() {    
+
+}
